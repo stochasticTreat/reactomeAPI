f7be427a86f5624c76b9a7624df0c38e1906d5c6
Samuel <samuelhiggins@gmail.com>
2014-07-01 17:27:01 -0700

    Working version
    Apparently the correct combo was to call the arguments style="HTTPPost", .opts=opts, and input=genes.str

--- CurlPostTest.R
+++ CurlPostTest.R
@@ -1,20 +1,12 @@
-library(rjson)
-library(RCurl)
+queryFIs2014attempt3<-function(url="http://www.reactome.org:80/AnalysisService/identifiers/projection?pageSize=20&page=1&sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL",
+							   genes=c("TP53","NOTCH1","RAS")){
+	library(rjson)
+	library(RCurl)
 
-genes=c("TP53","NOTCH1","RAS")
-genes.json = toJSON(x=genes)
-
-opts <- list(httpheader = c("Content-Type" = "text/plain;charset=UTF-8",
-                                                        "Accept" = "application/json"))
-#url obtained from http://www.reactome.org/AnalysisService/index.html
-#from the heading:  /identifiers/projection  Analise the post identifiers over the different species and projects the result to Homo Sapiens
-#by entering the above gene symbols and clicking the "Try it out!" button
-url="http://www.reactome.org:80/AnalysisService/identifiers/projection?pageSize=20&page=1&sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL"
-pres = postForm(uri=url, input=genes.json, .opts=opts, style="POST")
-
-#this result is obtained
-  # > pres
-  # [1] "{\"summary\":{\"token\":\"MjAxNDA3MDExOTU2MDlfMTUy\",\"type\":\"OVERREPRESENTATION\",\"sampleName\":null,\"species\":null,\"text\":true,\"fileName\":null},\"expression\":{\"columnNames\":[],\"min\":null,\"max\":null},\"identifiersNotFound\":1,\"pathwaysFound\":0,\"pathways\":[],\"resourceSummary\":[{\"resource\":\"TOTAL\",\"pathways\":0}]}"
-  # attr(,"Content-Type")
-  # 
-  # "application/json" 
+	genes.json <- toJSON(x=genes)
+	genes.str <- paste(genes, collapse = "\t")
+	opts <- list(httpheader = c("Content-Type" = "text/plain;charset=UTF-8",
+															"Accept" = "application/json"))
+	pres <- postForm(uri=url, input=genes.str, .opts=opts, style="HTTPPOST")
+	return(pres)
+}

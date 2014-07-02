638c2d3fedc6a44e79f175df2c6c7ae38c48b535
Samuel <samuelhiggins@gmail.com>
2014-07-02 01:28:57 -0700

    Missing identifiers and different results
    Demonstrates differences between API via R and API via http://www.reactome.org/AnalysisService/index.html 
    (10 paths returned via R where 18 paths were returned using the documentation example)
    Also demonstrates the empty response that is sometimes found when passing a set of genes with gene identifiers that cannot be found.

--- CurlPostTest.R
+++ CurlPostTest.R
@@ -1,4 +1,4 @@
-queryFIs2014attempt3<-function(url="http://www.reactome.org:80/AnalysisService/identifiers/projection?pageSize=20&page=1&sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL",
+queryReactomeAnalysisAPI<-function(url="http://www.reactome.org:80/AnalysisService/identifiers/projection?pageSize=8000&page=1&sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL",
 							   genes=c("TP53","NOTCH1","RAS")){
 	library(rjson)
 	library(RCurl)
@@ -10,3 +10,16 @@ queryFIs2014attempt3<-function(url="http://www.reactome.org:80/AnalysisService/i
 	pres <- postForm(uri=url, input=genes.str, .opts=opts, style="HTTPPOST")
 	return(pres)
 }
+
+gs1 = c('AKAP13', 'ALG6', 'ANK2')
+
+res1 = queryReactomeAnalysisAPI(genes=gs1)
+#this returns a set of results with 10 pathways in JSON format
+
+res3 = queryReactomeAnalysisAPI(genes=c('ABCA13', 'ABCG4', 'ACSM2B'))
+#this returns the following empty JSON response:
+### > restres
+### [1] "{\"summary\":{\"token\":\"MjAxNDA3MDIwNDIzNTFfMjIy\",\"type\":\"OVERREPRESENTATION\",\"sampleName\":null,\"species\":null,\"text\":true,\"fileName\":null},\"expression\":{\"columnNames\":[],\"min\":null,\"max\":null},\"identifiersNotFound\":4,\"pathwaysFound\":0,\"pathways\":[],\"resourceSummary\":[{\"resource\":\"TOTAL\",\"pathways\":0}]}"
+### attr(,"Content-Type")
+###                   
+### "application/json" 
